1cfe5c4cd6b37556e811111f1ad8117278a603ed
mosemos <mosemos12@gmail.com>
2019-05-17 06:42:00 +0200

    - added solution for 2.2. - added README.txt file. - converted all tabs to spaces

--- assignments/3/ngram_LM.py
+++ assignments/3/ngram_LM.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-# SNLP - SoSe 2019 - ASSINGMENT III 
+# SNLP - SoSe 2019 - ASSINGMENT III
 
 import math
 import re
@@ -9,172 +9,170 @@ from nltk import ngrams
 
 
 def tokenize(text):
-	"List all the word tokens (consecutive letters) in a text. Normalize to lowercase."
-	return re.findall('[a-z]+', text.lower())
+    "List all the word tokens (consecutive letters) in a text. Normalize to lowercase."
+    return re.findall('[a-z]+', text.lower())
 
 def word_ngrams(sent, n):
-	"""Givne a sent as str return n-grams as a list of tuple"""
-	
-	# EXAMPLES 
-	# > word_ngrams(tokenize('hello world'), 1)
-	# [('hello',), ('world',)]
-	# > word_ngrams(tokenize('hello world'), 2)
-	# [('<s>', 'hello'), ('hello', 'world'), ('world', '</s>')]
+    """Givne a sent as str return n-grams as a list of tuple"""
 
-	# YOUR CODE HERE
-	tokenized_sent = tokenize(sent)
-	if n != 1:
-		tokenized_sent.insert(0, '<s>')
-		tokenized_sent.append('</s>')
-	return [tuple(tokenized_sent[i:i + n]) for i in range(0, len(tokenized_sent)-n+1)]
+    # EXAMPLES
+    # > word_ngrams(tokenize('hello world'), 1)
+    # [('hello',), ('world',)]
+    # > word_ngrams(tokenize('hello world'), 2)
+    # [('<s>', 'hello'), ('hello', 'world'), ('world', '</s>')]
+
+    # YOUR CODE HERE
+    tokenized_sent = tokenize(sent)
+    if n != 1:
+        tokenized_sent.insert(0, '<s>')
+        tokenized_sent.append('</s>')
+    return [tuple(tokenized_sent[i:i + n]) for i in range(0, len(tokenized_sent)-n+1)]
 
 
 class ngram_LM:
-	"""A class to represent a language model."""
+    """A class to represent a language model."""
 
-	def __init__(self, n, ngram_counts, vocab, unk=False):
-		""""Make a n-gram language model, given a vocab and
-			data structure for n-gram counts."""
-		
-		self.n = n 
-		
-		self.vocab = vocab
-		
-		self.V = len(vocab)
+    def __init__(self, n, ngram_counts, vocab, unk=False):
+        """"Make a n-gram language model, given a vocab and
+            data structure for n-gram counts."""
 
-		self.ngram_counts = ngram_counts
+        self.n = n
 
-	# YOUR CODE HERE
-	# START BY MAKING THE RIGHT COUNTS FOR THIS PARTICULAR self.n 
-		# for unigrams, we only need total word count
-		if n == 1:
-			self.total_count = sum(self.ngram_counts.values())
-		# for bigrams, we need total count wrt each word. In our language, it is history count. 
-		elif n == 2:
-			self.history_count = Counter()
-			for k, v in self.ngram_counts.items():
-				self.history_count[k[0]] = self.history_count[k[0]] + v
-			# since we only count for the first word in the tuple, we will always
-			# miss counting </s>. However, since the frequency of </s> is the same
-			# as the frequency of <s>, we can simply assign it equal to it.
-			self.history_count['</s>'] = self.history_count['<s>']
+        self.vocab = vocab
 
+        self.V = len(vocab)
 
-		
-	   
-   
+        self.ngram_counts = ngram_counts
 
-	def estimate_prob(self, history, word):
-		"""Estimate probability of a word given a history."""
-		# YOUR CODE HERE
-
-		if history == '':
-			# unigram
-			word_frequency = self.ngram_counts[tuple([word])]
-			return word_frequency/self.total_count
-
-		else:
-			# bigram
-			word_frequency = self.ngram_counts[tuple([history, word])]
-			history_count = self.history_count[history]
-			if history_count == 0:
-				return 0
-			return word_frequency/history_count
-
-	
-	def estimate_smoothed_prob(self, history, word, alpha = 0.5):
-		"""Estimate probability of a word given a history with Lidstone smoothing."""
-
-		if history == '':
-			# unigram
-			word_frequency = self.ngram_counts[tuple([word])]
-			return (word_frequency + alpha)/(alpha*self.V +self.total_count)
-
-		else:
-			# bigram
-			word_frequency = self.ngram_counts[tuple([history, word])]
-			history_count = self.history_count[history]
-			# handle div by 0
-			if history_count == 0:
-				return 0
-			return (word_frequency + alpha)/(alpha*self.V + history_count)
-
-		
-	   
-	# YOUR CODE HERE
-			
-
-	def logP(self, history, word):
-		"""Return base-2 log probablity."""
-
-	# YOUR CODE HERE
-				 
-
-
-	def score_sentence(self, sentence):
-		"""Given a sentence, return score."""
-		
-		# YOUR CODE HERE
-
-
- 
-	def test_LM(self):
-		"""Test whether or not the probability mass sums up to one."""
-		
-		precision = 10**-8
-				 
-		if self.n == 1:
-				 
-			P_sum = sum(self.estimate_prob('', w) for w in self.vocab)
-			
-			assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one.'
-				 
-		elif self.n == 2:
-			histories = ['the', 'in', 'at', 'blue', 'white']
-				 
-			for h in histories:
-				 
-				P_sum = sum(self.estimate_prob(h, w) for w in self.vocab)
-				
-				assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one for history' + h
-					 
-		print('TEST SUCCESSFUL!')
+    # YOUR CODE HERE
+    # START BY MAKING THE RIGHT COUNTS FOR THIS PARTICULAR self.n
+        # for unigrams, we only need total word count
+        if n == 1:
+            self.total_count = sum(self.ngram_counts.values())
+        # for bigrams, we need total count wrt each word. In our language, it is history count.
+        elif n == 2:
+            self.history_count = Counter()
+            for k, v in self.ngram_counts.items():
+                self.history_count[k[0]] = self.history_count[k[0]] + v
+            # since we only count for the first word in the tuple, we will always
+            # miss counting </s>. However, since the frequency of </s> is the same
+            # as the frequency of <s>, we can simply assign it equal to it.
+            self.history_count['</s>'] = self.history_count['<s>']
 
 
 
-	def test_smoohted_LM(self):
-		"""Test whether or not the smoothed probability mass sums up to one."""
-		precision = 10**-8
-				 
-		if self.n == 1:
-				 
-			P_sum = sum(self.estimate_smoothed_prob('', w) for w in self.vocab)
-			
-			assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one.'
-				 
-		elif self.n == 2:
-			histories = ['the', 'in', 'at', 'blue', 'white']
-				 
-			for h in histories:
-				 
-				P_sum = sum(self.estimate_smoothed_prob(h, w) for w in self.vocab)
-				
-				assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one for history' + h
-					 
-		print('TEST SUCCESSFUL!')
-		# YOUR CODE HERE
+
+
+
+    def estimate_prob(self, history, word):
+        """Estimate probability of a word given a history."""
+        # YOUR CODE HERE
+
+        if history == '':
+            # unigram
+            word_frequency = self.ngram_counts[tuple([word])]
+            return word_frequency/self.total_count
+
+        else:
+            # bigram
+            word_frequency = self.ngram_counts[tuple([history, word])]
+            history_count = self.history_count[history]
+            if history_count == 0:
+                return 0
+            return word_frequency/history_count
+
+
+    def estimate_smoothed_prob(self, history, word, alpha = 0.5):
+        """Estimate probability of a word given a history with Lidstone smoothing."""
+
+        if history == '':
+            # unigram
+            word_frequency = self.ngram_counts[tuple([word])]
+            return (word_frequency + alpha)/(alpha*self.V +self.total_count)
+
+        else:
+            # bigram
+            word_frequency = self.ngram_counts[tuple([history, word])]
+            history_count = self.history_count[history]
+            # handle div by 0
+            if history_count == 0:
+                return 0
+            return (word_frequency + alpha)/(alpha*self.V + history_count)
+
+
+    def logP(self, history, word):
+        """Return base-2 log probablity."""
+        prob = self.estimate_smoothed_prob(history, word)
+        log_prob = math.log(prob, 2)
+        return log_prob
+
+
+    def score_sentence(self, sentence):
+        """Given a sentence, return score."""
+        log_prob_sum = 0
+        for i in range(len(sentence)):
+            history = sentence[i][0]
+            word = sentence[i][1]
+            log_prob = self.logP(history, word)
+            log_prob_sum += log_prob
+        normalized_log_prob_sum = (-1 / len(sentence)) * log_prob_sum
+        return normalized_log_prob_sum
+
+
+    def test_LM(self):
+        """Test whether or not the probability mass sums up to one."""
+
+        precision = 10**-8
+
+        if self.n == 1:
+
+            P_sum = sum(self.estimate_prob('', w) for w in self.vocab)
+
+            assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one.'
+
+        elif self.n == 2:
+            histories = ['the', 'in', 'at', 'blue', 'white']
+
+            for h in histories:
+
+                P_sum = sum(self.estimate_prob(h, w) for w in self.vocab)
+
+                assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one for history' + h
+
+        print('TEST SUCCESSFUL!')
+
+
+
+    def test_smoohted_LM(self):
+        """Test whether or not the smoothed probability mass sums up to one."""
+        precision = 10**-8
+
+        if self.n == 1:
+
+            P_sum = sum(self.estimate_smoothed_prob('', w) for w in self.vocab)
+
+            assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one.'
+
+        elif self.n == 2:
+            histories = ['the', 'in', 'at', 'blue', 'white']
+
+            for h in histories:
+
+                P_sum = sum(self.estimate_smoothed_prob(h, w) for w in self.vocab)
+
+                assert abs(1.0 - P_sum) < precision, 'Probability mass does not sum up to one for history' + h
+
+        print('TEST SUCCESSFUL!')
+        # YOUR CODE HERE
 
 
 # # ADD YOUR CODE TO COLLECT COUTNS AND CONSTRUCT VOCAB
 
 
-# # ONCE YOU HAVE N-GRAN COUNTS AND VOCAB, 
-# # YOU CAN BUILD LM OBJECTS AS ... 
+# # ONCE YOU HAVE N-GRAN COUNTS AND VOCAB,
+# # YOU CAN BUILD LM OBJECTS AS ...
 # unigram_LM = ngram_LM(1, unigram_frequencies, vocabulary)
 
 
 # # THEN TEST YOUR IMPLEMENTATION AS ..
-# unigram_LM.test_LM() 
-	
-
-
+# unigram_LM.test_LM()
--- assignments/3/q2.py
+++ assignments/3/q2.py
@@ -17,74 +17,93 @@ def print_top_n(dictionary, top_n, message=""):
     print('\n--------------------\n\n')
 
 def calculate_ttr(ngram_counter):
-	types = len(ngram_counter.keys())
-	tokens = sum(ngram_counter.values())
-	return (types/tokens) * 100
+    types = len(ngram_counter.keys())
+    tokens = sum(ngram_counter.values())
+    return (types/tokens) * 100
 
 
 def main():
 
-	file_name = sys.argv[1]
-	f = open(file_name, "r", encoding="utf8")
+    file_name = sys.argv[1]
+    f = open(file_name, "r", encoding="utf8")
 
-	# ex 2.1 b
-	unigrams = []
-	bigrams = []
-	sentence_counter = 0
-	for sentence in f:
-		unigrams.extend(lm.word_ngrams(sentence, 1))
-		bigrams.extend(lm.word_ngrams(sentence, 2))
-		sentence_counter = sentence_counter +1
+    # ex 2.1 b
+    unigrams = []
+    bigrams = []
+    sentence_counter = 0
+    for sentence in f:
+        unigrams.extend(lm.word_ngrams(sentence, 1))
+        bigrams.extend(lm.word_ngrams(sentence, 2))
+        sentence_counter = sentence_counter +1
 
-	unigram_frequencies = Counter(unigrams)
-	unigrams = None
-	print_top_n(unigram_frequencies, 15, 'The top 15 unigrams are: ')
+    unigram_frequencies = Counter(unigrams)
+    unigrams = None
+    print_top_n(unigram_frequencies, 15, 'The top 15 unigrams are: ')
 
-	bigram_frequencies = Counter(bigrams)
-	bigrams = None
-	print_top_n(bigram_frequencies, 15, 'The top 15 bigrams are: ')
-
-
-	# 2.1 c
-	unigram_vocabulary = list(unigram_frequencies.keys())
-	unigram_vocabulary = [i[0] for i in unigram_vocabulary]
-
-	bigram_vocabulary = [i for i in unigram_vocabulary]
-	bigram_vocabulary.extend(['<s>','</s>'])
-
-	print('The ttr is {}'.format(calculate_ttr(unigram_frequencies)))
-	
-	# 2.1 d
-	t1 = time.time()
-	unigram_LM = lm.ngram_LM(1, unigram_frequencies, unigram_vocabulary)
-	t2 = time.time()
-	print('unigram LM made in {} secs'.format(t2-t1))
-	print(unigram_LM.estimate_prob('', 'and'))
-	unigram_LM.test_LM()
-	t3 = time.time()
-	print('unigram LM tested in {} secs'.format(t3-t2))
-
-	bigram_LM = lm.ngram_LM(2, bigram_frequencies, bigram_vocabulary)
-	t4 = time.time()
-	print('bigram LM made in {} secs'.format(t4 - t3))
-	print(bigram_LM.estimate_prob('of', 'the'))
-	bigram_LM.test_LM()
-	print('bigram LM tested in {} secs'.format(time.time() - t4))
+    bigram_frequencies = Counter(bigrams)
+    bigrams = None
+    print_top_n(bigram_frequencies, 15, 'The top 15 bigrams are: ')
 
 
-	# 2.1 e
-	histories = ['blue', 'green', 'white', 'black', 'natural', 'artificial', 'global', 'domestic']
-	for history in histories:
-		history_prob_dist = defaultdict()
-		for word in bigram_LM.vocab:
-			history_prob_dist[word] = bigram_LM.estimate_prob(history, word)
-		print_top_n(history_prob_dist, 10, 'The top 10 for history {} are:'.format(history))
+    # 2.1 c
+    unigram_vocabulary = list(unigram_frequencies.keys())
+    unigram_vocabulary = [i[0] for i in unigram_vocabulary]
+
+    bigram_vocabulary = [i for i in unigram_vocabulary]
+    bigram_vocabulary.extend(['<s>','</s>'])
+
+    print('The ttr is {}'.format(calculate_ttr(unigram_frequencies)))
+
+    # 2.1 d
+    t1 = time.time()
+    unigram_LM = lm.ngram_LM(1, unigram_frequencies, unigram_vocabulary)
+    t2 = time.time()
+    print('unigram LM made in {} secs'.format(t2-t1))
+    print(unigram_LM.estimate_prob('', 'and'))
+    unigram_LM.test_LM()
+    t3 = time.time()
+    print('unigram LM tested in {} secs'.format(t3-t2))
+
+    bigram_LM = lm.ngram_LM(2, bigram_frequencies, bigram_vocabulary)
+    t4 = time.time()
+    print('bigram LM made in {} secs'.format(t4 - t3))
+    print(bigram_LM.estimate_prob('of', 'the'))
+    bigram_LM.test_LM()
+    print('bigram LM tested in {} secs'.format(time.time() - t4))
 
 
-	# 2.2 b
-	unigram_LM.test_smoohted_LM()
-	bigram_LM.test_smoohted_LM()
+    # 2.1 e
+    histories = ['blue', 'green', 'white', 'black', 'natural', 'artificial', 'global', 'domestic']
+    for history in histories:
+        history_prob_dist = defaultdict()
+        for word in bigram_LM.vocab:
+            history_prob_dist[word] = bigram_LM.estimate_prob(history, word)
+        print_top_n(history_prob_dist, 10, 'The top 10 for history {} are:'.format(history))
+
+
+    # 2.2 b
+    unigram_LM.test_smoohted_LM()
+    bigram_LM.test_smoohted_LM()
+
+    # 2.2 c
+    translation1 = 'Yesterday was I at home'
+    translation2 = 'Yesterday I was at home'
+    translation3 = 'I was at home yesterday'
+
+    ngrams1 = lm.word_ngrams(translation1, 2)
+    ngrams2 = lm.word_ngrams(translation2, 2)
+    ngrams3 = lm.word_ngrams(translation3, 2)
+
+    score1 = bigram_LM.score_sentence(ngrams1)
+    score2 = bigram_LM.score_sentence(ngrams2)
+    score3 = bigram_LM.score_sentence(ngrams3)
+
+    print('Score of "Yesterday was I at home": ', score1)
+    print('Score of "Yesterday I was at home":', score2)
+    print('Score of "I was at home yesterday":', score3)
+
+
 
 
 if __name__ == "__main__":
-	main()
+    main()
